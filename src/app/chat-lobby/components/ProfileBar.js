c83f9145b303cc98d99c37426e9370a611ccac09
Binuwalker <sambinudickson05@gmail.com>
2024-11-06 18:29:22 +0530

    Initial ommit

--- src/app/chat-lobby/components/ProfileBar.js
+++ src/app/chat-lobby/components/ProfileBar.js
@@ -1,7 +1,10 @@
 "use client";
+import { AUTH_LOGIN_PAGE } from "@/helpers/paths";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const ProfileBar = ({ userData }) => {
+  const router = useRouter();
   return (
     <div className="bg-[var(--background-beta)] h-[70px] w-full">
       <div className="flex items-center justify-between w-full px-4 h-full">
@@ -16,7 +19,13 @@ const ProfileBar = ({ userData }) => {
             className="object-cover h-12 w-12 rounded-full"
           />
           <div>{userData?.username}</div>
-          <button className="bg-[var(--background-alpha)] text-xs p-2">
+          <button
+            className="bg-[var(--background-alpha)] text-xs p-2"
+            onClick={() => {
+              localStorage.removeItem("jwt_token");
+              router.push(AUTH_LOGIN_PAGE);
+            }}
+          >
             Logout
           </button>
         </div>
